19d1581166d065d3bad5fbd1f1534b49479a7382
MaxDevv <maximusadeola@gmail.com>
2025-04-16 17:26:29 +0000

    Addedd Formatted Posts :D

--- Step-2 Extracting symptoms/main.py
+++ Step-2 Extracting symptoms/main.py
@@ -7,7 +7,7 @@ Module Description
 A brief description of what this module/script does.
 """
 
-import sys, json, os
+import sys, json, os, re
 from typing import Optional, Any
 from vertexai.preview import tokenization
 
@@ -72,7 +72,6 @@ class MainClass:
         return cnt
     
     
-    
     def countTokens(self, text: str) -> int:
         """
         Description:
@@ -86,7 +85,54 @@ class MainClass:
             self.tokenizer = tokenization.get_tokenizer_for_model("gemini-1.5-flash-001")
         return self.tokenizer.count_tokens(text).total_tokens
 
+    def formatPost(self, postIdx: int) -> str:
+        """
+        Description:
+            - Formats a post into this format:
+                ""Post 1: "{Insert Title}"
+                    Body: "{Insert Body}"
+                    Comments:
+                        Comment 1.1: "Some Comment"
+                        Comment 1.2: "Some Comment"
+                ""
+        Args:
+            - variable,  an int representing a variable
+        """
+        def deEmojify(text):
+            emoj = re.compile("["
+                u"\U0001F600-\U0001F64F"  # emoticons
+                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                u"\U00002500-\U00002BEF"  # chinese char
+                u"\U00002702-\U000027B0"
+                u"\U000024C2-\U0001F251"
+                u"\U0001f926-\U0001f937"
+                u"\U00010000-\U0010ffff"
+                u"\u2640-\u2642" 
+                u"\u2600-\u2B55"
+                u"\u200d"
+                u"\u23cf"
+                u"\u23e9"
+                u"\u231a"
+                u"\ufe0f"  # dingbats
+                u"\u3030"
+                            "]+", re.UNICODE)
+            return re.sub(emoj, '', text)
 
+        def cleanText(text):
+            return deEmojify(text.replace("\n", " ").replace('\u2019', "'").replace('\u00a0', " ").replace('\u2013', "-").replace('\u2018', "'").replace('\u201c', '"').replace('\u201d', '"').replace('\u2026', "...").replace('\ud83d\ude02', "").replace('\u2763\ufe0f', "").replace('\ud83d\udc95', ""))
+
+        if self.verbose:
+            print(f"Formatting post ({postIdx+1})...")
+        post = self.dump[postIdx]
+        post = f"""Post {postIdx+1}: "{cleanText(post["title"])}"
+    Body: "{cleanText(post["text"])}"
+    Comments:
+{"\n".join([f'        Comment {postIdx+1}.{idx+1}: "{cleanText(comment)}"' for idx, comment in enumerate(post["comments"])])}
+"""
+        return post
+        
     def getTotTokens(self) -> int:
         """
         Description:
@@ -100,18 +146,18 @@ class MainClass:
         self.wasVerbose = self.verbose
         self.verbose = False
         for post in self.dump:
-            # cnt += self.countTokens(post["title"])
-            # cnt += self.countTokens(post["text"])
+            cnt += self.countTokens(post["title"])
+            cnt += self.countTokens(post["text"])
             for comment in post["comments"]:
                 cnt += self.countTokens(comment)
         self.verbose = self.wasVerbose
-
-        print(cnt)
         
-        print("Total tokens counted:", cnt)
+        if self.verbose:
+            print("Total tokens counted:", cnt)
 
         return cnt
 
+    # def extractSymptoms(self, )
         
     def run(self) -> None:
         """
@@ -122,8 +168,32 @@ class MainClass:
         """
         self.loadDump()
         # print(self.countComments())
-        print(self.getTotTokens())
-        pass
+
+        # lets test some chunk sizes, I want stuff that will fit into gemini's context window, Ik google yaps about a 1 million context window, but companies brag and exaggerate all the time so ill go for like ~300k tokens
+        # chunk = "\n".join([self.formatPost(i) for i in range(100)])
+        # self.verbose = False
+        # print(
+        #     self.countTokens("\n".join([self.formatPost(i) for i in range(100)])),
+        #     self.countTokens("\n".join([self.formatPost(i) for i in range(100, 215)])),
+        #     self.countTokens("\n".join([self.formatPost(i) for i in range(215, 275)])),
+        #     self.countTokens("\n".join([self.formatPost(i) for i in range(275, 375)])),
+        #     self.countTokens("\n".join([self.formatPost(i) for i in range(375, 494)])),
+        # )
+        # exit()
+        # Took some finnessing but I managed to break it up into 5 relatively equal chunks of around 260k tokens
+
+        # Token Sizes, In Order: 256987 268988 253182 288039 288417
+        # chunks = [
+        #     "\n".join([self.formatPost(i) for i in range(000, 100)]),
+        #     "\n".join([self.formatPost(i) for i in range(100, 215)]),
+        #     "\n".join([self.formatPost(i) for i in range(215, 275)]),
+        #     "\n".join([self.formatPost(i) for i in range(275, 375)]),
+        #     "\n".join([self.formatPost(i) for i in range(375, 494)]),
+        # ]
+        # print(self.countTokens("\n".join([self.formatPost(i) for i in range(000, 494)])))
+        # with open(os.path.join(Config.SCRIPT_DIR, "formatted-posts.txt"), "w+") as f:
+        #     f.write("\n".join([self.formatPost(i) for i in range(000, 494)]))
+
 
 # ===== Main Function =====
 def main() -> int:
